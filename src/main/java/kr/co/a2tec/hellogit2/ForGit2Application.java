b9e737495ebf6f71112f7d268b19c2ebc5a972c2
hjjeon <hjjeon@a2tec.co.kr>
2022-02-03 17:49:22 +0900

    프로젝트 생성

---
+++ src/main/java/kr/co/a2tec/hellogit2/ForGit2Application.java
@@ -0,0 +1,13 @@
+package kr.co.a2tec.hellogit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForGit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForGit2Application.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/a2tec/hellogit2/ForGit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.a2tec.hellogit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForGit2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
